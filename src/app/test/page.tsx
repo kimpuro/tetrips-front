b239f29636fa610acffb32527ec2c9da081511f1
KimPuro <puro@protonmail.ch>
2024-07-18 17:19:22 +0900

    refactor : 테스트 코드 리팩터링

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,35 +1,27 @@
-'use client'
+'use client';
 import React, { useState } from 'react';
 
 export default function ParentComponent() {
   const [count, setCount] = useState(0);
 
-  const count1 = () => {
-    setCount(1);
-  };
-  const count2 = () => {
-    setCount(2);
-  };
-  const count3 = () => {
-    setCount(3);
+  const changeCount = (value:number) => {
+    setCount(prevCount => value); // Update count correctly
   };
 
   return (
     <div>
       <p>Count: {count}</p>
-      <ChildComponent onIncrement={count1} />
-      <ChildComponent onIncrement={count2} />
-      <ChildComponent onIncrement={count3} />
+      <ChildComponent onIncrement={() => changeCount(1)} />
+      <ChildComponent onIncrement={() => changeCount(2)} />
+      <ChildComponent onIncrement={() => changeCount(3)} />
     </div>
   );
 }
 
 function ChildComponent({ onIncrement } : any) {
   return (
-    <>
     <button onClick={onIncrement}>
       Increment
     </button>
-    </>
   );
 }
