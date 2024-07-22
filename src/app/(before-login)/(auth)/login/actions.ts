08f1b9aa14965df1acd597e444b73c5199d2fb0d
KimPuro <puro@protonmail.ch>
2024-07-22 16:48:14 +0900

    feat : 로그인 패치 요청 구현

--- src/app/(before-login)/(auth)/login/actions.ts
+++ src/app/(before-login)/(auth)/login/actions.ts
@@ -1,9 +1,21 @@
 'use server'
 
 export async function login(formData: FormData) {
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
+  try {
+    const data = {
+      username: formData.get('email') as string,
+      password: formData.get('password') as string,
+    }
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+    console.log(res)
+    return res.json();
+  } catch (error) {
+    return error
   }
-  console.log(data)
 }
