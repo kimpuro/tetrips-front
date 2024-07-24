ab2177c5bf770e5b589ecb5b6979d9c8533267d3
KimPuro <puro@protonmail.ch>
2024-07-24 17:33:59 +0900

    test : 클라이언트 컴포넌트에스 set-cookie 테스트

--- src/app/(before-login)/(auth)/login/actions.ts
+++ src/app/(before-login)/(auth)/login/actions.ts
@@ -4,24 +4,22 @@ import { redirect } from 'next/navigation'
 
 export async function login(formData: FormData) {
   try {
-    const data = {
-      username: formData.get('email') as string,
-      password: formData.get('password') as string,
-    }
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/user/login`, {
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
+    console.log("보내는 데이터", email, password);
+
+    // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login/local`, {
+    const res = await fetch(`http://localhost:3000/test/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),
-    })
+      body: JSON.stringify({ email, password }),
+      credentials: 'include', // 쿠키 포함 옵션 추가
+    });
     console.log(res)
-    return res.json();
   } catch (error) {
     console.log(error)
-    if(error){
       redirect('error');
-    }
-    return error
   }
 }
--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,30 +1,33 @@
 'use client';
-import React, { useState } from 'react';
 
-export default function ParentComponent() {
-  const [count, setCount] = useState(0);
+import { redirect } from 'next/navigation'
 
-  const changeCount = (value:number) => {
-    setCount(prevCount => value); // Update count correctly
-  };
+export default function Page() {
+  const fetchLogin = async () => {
+    try {
+      const email = 'qwerty@qwerty.com'
+      const password = '1234'
+      console.log("보내는 데이터", email, password);
+
+      // const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/login/local`, {
+      const res = await fetch(`http://localhost:3000/test/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+        credentials: 'include', // 쿠키 포함 옵션 추가
+      });
+      console.log(res)
+    } catch (error) {
+      console.log(error)
+      redirect('error');
+    }
+  }
 
   return (
-    <div>
-      <p>Count: {count}</p>
-      <ChildComponent onIncrement={() => changeCount(1)} />
-      <ChildComponent onIncrement={() => changeCount(2)} />
-      <ChildComponent onIncrement={() => changeCount(3)} />
+    <div className="bg-cyan-400" onClick={fetchLogin}>
+      로그인 테스트 버튼
     </div>
-  );
-}
-
-function ChildComponent({ onIncrement } : any) {
-  return (
-    <>
-    <button onClick={onIncrement}>
-      Increment
-    </button>
-      <br/>
-      </>
   );
 }
