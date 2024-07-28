235f8950d0c2902fb79ff67945127e27b3f93eff
KimPuro <puro@protonmail.ch>
2024-07-28 23:03:00 +0900

    fix : 로그아웃 용도의 페이지 삭제

--- src/app/(auth)/mypage/page.tsx
+++ src/app/(auth)/mypage/page.tsx
@@ -2,10 +2,8 @@
 export default function Page(){
 
   return (
-    <div onClick={async () => {
-      await deleteCookie('cookieName', { path: '/' });
-    }}>
-      <h1>로그아웃 되었습니다.</h1>
+    <div>
+      <h1>마이 페이지</h1>
     </div>
   )
 }
